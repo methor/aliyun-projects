0e73d148e189e27577f2ee3477a70e2a86589b02
hengxin <hengxin0912@gmail.com>
2016-07-15 11:02:52 +0800

    +aliccd: not finished; to exclude cassandra seeds

--- aliyun-scripts/alic-scripts/aliccd.sh
+++ aliyun-scripts/alic-scripts/aliccd.sh
@@ -20,33 +20,46 @@
 set -o nounset                              # Treat unset variables as an error
 
 function usage() {
-    echo "aliccd -t='hosts' -s='seeds'"
+    echo "aliccd -h='hosts' -s='seeds' -p='port' -t='tag' -n='name'"
     echo ""
-    echo "This script ."
+    echo "This script launches a Cassandra of 'tag' cluster on 'hosts' with 'seeds' as seeds."
     echo ""
-    echo "-h --help"
-    echo "-t --hosts. Default: 'nc1 nc2 nc3 sc1 sc2 sc3 ec1 ec2 ec3'."
+	echo "WARNING: force removal ... (to do)"
+    echo "--help: print usage."
+    echo "-h --hosts. Default: 'nc1 nc2 nc3 sc1 sc2 sc3 ec1 ec2 ec3'."
 	echo "-s --seeds. Default: 'nc1 sc1 ec1'."
+	echo "-p --ports. Default: '7000'."
+	echo "-t --tag. Default: 'latest'"
+	echo "-n --name. Default: ''"
     echo ""
 }
 
 hosts="nc1 nc2 nc3 sc1 sc2 sc3 ec1 ec2 ec3"	# run on all 9 ECSes
+seeds="nc1 sc1 ec1"  # seeds in different regions
+port="7000"  # port for gossip
+tag="latest"  # cassandra:latest
 
-# parse the arguments: -h (--help), -t (--hosts), -s (--seeds)
+# parse the arguments: --help, -h (--hosts), -s (--seeds), -t (--tag)
 while [ "$1" != "" ]; do
     PARAM=`echo $1 | awk -F= '{print $1}'`
     VALUE=`echo $1 | awk -F= '{print $2}'`
 
     case $PARAM in
-        -h | --help)
+        --help)
             usage
             exit
             ;;
-	    -t | --hosts)
+	    -h | --hosts)
 	    	hosts=$VALUE
 	    	;;
 		-s | --seeds)
 			seeds=$VALUE
+			;;
+		-p |--port)
+			port=$VALUE
+			;;
+		-t | --tag)
+			tag=$VALUE
 			;;
         *)
             echo "ERROR: unknown parameter \"$PARAM\""
@@ -57,6 +70,39 @@ while [ "$1" != "" ]; do
     shift
 done
 
-# first starting the seeds
+# first starting the seeds in 'seeds'
 
-# then starting the others
+for seed in $seeds; do
+	name=naming $seed
+	ip=getIP $seed
+
+	run_cmd="docker run --name $name -d -e CASSANDRA_BROADCAST_ADDRESS=$ip -p $port:$port cassandra:$tag"
+	rm_cmd="docker rm -f $name"
+	$run_cmd || ($rm_cmd && $run_cmd)
+done
+
+# then starting the others in 'hosts' (excluding those also in 'seeds')
+
+
+for host in $hosts; do
+	name=naming $host
+	ip=getIP $host
+
+	if [  ] ; then  # excluding those in 'seeds'
+		# choose a random seed
+		seed_ip=getIP ${seeds[$RANDOM % ${#seeds[@]} ]}
+		run_cmd="docker run --name $name -d -e CASSANDRA_BROADCAST_ADDRESS=$ip -p $port:$port -e CASSANDRA_SEEDS=$seed_ip cassandra:$tag"
+		rm_cmd='docker rm -f $name'
+		$run_cmd || ($rm_cmd && $run_cmd)
+	fi
+done
+
+function naming() {
+	local h="$1"
+	echo 'cassandra'-'$h'-'cluster'-'$name'
+}
+
+function getIP() {
+	local h="$1"
+    echo getent hosts $seed awk 'NR==1 {print $1}'
+}
